9715cce448bcc29544b17d14c10b36a3d8d7fd68
magician8421 <83378122@163.com>
2024-10-09 10:28:37 +0800

    2041009

--- 20241009/Call.sol
+++ 20241009/Call.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
+import "https://github.com/NomicFoundation/hardhat/blob/main/packages/hardhat-core/console.sol";
 
 contract Called{
      uint256 public number;
@@ -30,11 +31,17 @@ contract Caller{
     function setN(uint256 _number) external {
       (bool _r,bytes memory data)= to.call(abi.encodeWithSignature("setN(uint256)",_number));
        require(_r,"execute fail");
+       console.logBytes(data);
     }
 
     function delegateSetN(uint256 _number) external {
       (bool _r,bytes memory data)= to.delegatecall(abi.encodeWithSignature("setN(uint256)",_number));
        require(_r,"execute fail");
+       console.logBytes(data);
     }
-   
+     function staticSetN() view external {
+      (bool _r,bytes memory data)= to.staticcall(abi.encodeWithSignature("number()"));
+       require(_r,"execute fail");
+       console.logBytes(data);
+    }
 }
